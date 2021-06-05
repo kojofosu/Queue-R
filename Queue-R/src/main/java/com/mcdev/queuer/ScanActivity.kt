4d1964a70d3d8e765af4ab873d2614482849d8ab
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-06-05 15:26:04 +0000

    setting flash [in progress]

--- Queue-R/src/main/java/com/mcdev/queuer/ScanActivity.kt
+++ Queue-R/src/main/java/com/mcdev/queuer/ScanActivity.kt
@@ -1,11 +1,73 @@
 package com.mcdev.queuer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceHolder
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import com.mcdev.queuer.databinding.ActivityScanBinding
+import java.io.IOException
+
 
 class ScanActivity : AppCompatActivity() {
+    private val TAG = "ScanActivity"
+    private lateinit var binding: ActivityScanBinding
+    private val REQUEST_CAMERA_PERMISSION: Int = 200
+    private lateinit var barcodeDetector: BarcodeDetector
+    private lateinit var cameraSource: CameraSource
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan)
+        binding = ActivityScanBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        barcodeDetector = BarcodeDetector.Builder(this)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
+
+        cameraSource = CameraSource.Builder(this, barcodeDetector)
+            .setRequestedPreviewSize(1920, 1080)
+            .setAutoFocusEnabled(true)
+            .build()
+
+        binding.scanSurfaceView.holder.addCallback(object : SurfaceHolder.Callback{
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                try {
+                    cameraSource.start(binding.scanSurfaceView.holder)
+//                    if (ActivityCompat.checkSelfPermission(
+//                            this@ScanActivity, Manifest.permission.CAMERA) ==
+//                                    PackageManager.PERMISSION_GRANTED
+//                        ){
+//                        cameraSource.start(binding.scanSurfaceView.holder)
+//                    }else{
+////                        ActivityCompat.requestPermissions(this@ScanActivity,
+////                            String[]{ Manifest.permission.CAMERA }, REQUEST_CAMERA_PERMISSION)
+//                    }
+                } catch (e: IOException) {
+                    Log.d(TAG, "surfaceCreated: $e")
+                }
+
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+
+        })
     }
 }

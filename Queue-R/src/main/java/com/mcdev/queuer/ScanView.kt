5f628ade87b0640c78b8ebc0f8d9f2eaa43c625a
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-06-09 14:40:31 +0000

    Adds flashlight

--- Queue-R/src/main/java/com/mcdev/queuer/ScanView.kt
+++ Queue-R/src/main/java/com/mcdev/queuer/ScanView.kt
@@ -2,24 +2,145 @@ package com.mcdev.queuer
 
 
 import android.content.Context
+import android.graphics.ImageFormat
+import android.hardware.Camera
+import android.hardware.camera2.*
+import android.media.ImageReader
+import android.os.Handler
 import android.util.AttributeSet
+import android.util.Log
 import android.view.*
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.mcdev.queuer.databinding.ScanViewBinding
+import java.lang.Compiler.enable
 
 
-class ScanView(context: Context,
-               attrs: AttributeSet? = null,
-    ): ConstraintLayout (context, attrs) {
+class ScanView(
+    context: Context,
+    attrs: AttributeSet? = null,
+): ConstraintLayout (context, attrs) {
 
     private val TAG = "ScanView"
     private val layoutInflater = LayoutInflater.from(context)
     private val binding = ScanViewBinding.inflate(layoutInflater, this, true)
 
+    private lateinit var surfaceHolder: SurfaceHolder
+    private var isFlashEnabled: Boolean = false
 
     init {
-        binding.galleryImageButton.setOnClickListener { Toast.makeText(context, "First yu", Toast.LENGTH_LONG).show() }
+        binding.galleryImageButton.setOnClickListener { /*TODO not yet implemented*/ }
+        binding.flashImageButton.setOnClickListener {
+            if (isFlashEnabled) {
+                isFlashEnabled = false
+                startCameraPreview(isFlashEnabled)
+            } else {
+                isFlashEnabled = true
+                startCameraPreview(isFlashEnabled)
+            }
+        }
+        camera()
     }
 
+    fun camera() {
+        binding.scanSurfaceView.holder.addCallback(object: SurfaceHolder.Callback{
+            override fun surfaceCreated(holder: SurfaceHolder) {
+//                startCameraPreview2(false)
+
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+//                startCameraPreview(1920, 1080, false)
+                }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+            }
+        })
+
+    }
+
+    private fun startCameraPreview(enableFlash: Boolean) {
+        try {
+            // TODO
+            val cameraBkgHandler = Handler()
+
+            val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+            cameraManager.cameraIdList.find {
+                val characteristics = cameraManager.getCameraCharacteristics(it)
+                val cameraDirection = characteristics.get(CameraCharacteristics.LENS_FACING)
+
+                return@find cameraDirection != null && cameraDirection == CameraCharacteristics.LENS_FACING_BACK
+            }?.let {
+                val cameraStateCallback = object : CameraDevice.StateCallback() {
+                    override fun onOpened(camera: CameraDevice) {
+                        val captureStateCallback = object : CameraCaptureSession.StateCallback() {
+                            override fun onConfigured(session: CameraCaptureSession) {
+                                var builder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                                builder.addTarget(binding.scanSurfaceView.holder.surface)
+
+                                if (enableFlash) { builder = enableFlashMode(builder, enableFlash) }
+
+                                session.setRepeatingRequest(builder.build(), null, null)
+
+                                val request = builder.build()
+                                session.capture(request, null, null)
+                            }
+
+                            override fun onConfigureFailed(session: CameraCaptureSession) {
+                                // TODO
+                                Log.d(TAG, "onConfigureFailed")
+                            }
+                        }
+
+                        camera.createCaptureSession(
+                            listOf(binding.scanSurfaceView.holder.surface),
+//                            listOf(surfaceHolder.surface),
+                            captureStateCallback,
+                            cameraBkgHandler
+                        )
+                    }
+
+                    override fun onClosed(camera: CameraDevice) {
+                        // TODO
+                        Log.d(TAG, "onClosed")
+                    }
+
+                    override fun onDisconnected(camera: CameraDevice) {
+                        // TODO
+                        Log.d(TAG, "onDisconnected")
+                    }
+
+                    override fun onError(camera: CameraDevice, error: Int) {
+                        // TODO
+                        Log.d(TAG, "onError")
+                    }
+                }
+
+                cameraManager.openCamera(it, cameraStateCallback, cameraBkgHandler)
+                return
+            }
+
+            // TODO: - No available camera found case
+
+        } catch (e: CameraAccessException) {
+            // TODO
+            Log.e(TAG, e.message!!)
+        } catch (e: SecurityException) {
+            // TODO
+            Log.e(TAG, e.message!!)
+        }
+    }
+
+
+    /*Enables Flash light*/
+    fun enableFlashMode(builder: CaptureRequest.Builder, mode: Boolean): CaptureRequest.Builder {
+        builder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_TORCH)
+        builder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON)
+        return builder
+    }
 }
--- app/src/main/java/com/mcdev/queuer/MainActivity.kt
+++ app/src/main/java/com/mcdev/queuer/MainActivity.kt
@@ -13,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+//        binding.scanView.camera()
     }
 }
